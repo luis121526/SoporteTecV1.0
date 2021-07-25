db8d3e6c9f314778cdb309e464f93c870728157c
luis121526 <diazdiaz121526@gmail.com>
2021-07-25 01:13:27 -0500

    dasdsa

---
+++ Contenido/menu.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/propio.css">
+    <title>Menu Service Desk</title>
+</head>
+<body>
+    <div class="row container-fluid">
+        <div class="col-2">
+            <h6><span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-windows" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M17.8 20l-12 -1.5c-1 -.1 -1.8 -.9 -1.8 -1.9v-9.2c0 -1 .8 -1.8 1.8 -1.9l12 -1.5c1.2 -.1 2.2 .8 2.2 1.9v12.1c0 1.2 -1.1 2.1 -2.2 1.9z" />
+                <line x1="12" y1="5" x2="12" y2="19" />
+                <line x1="4" y1="12" x2="20" y2="12" />
+                </svg>
+            </span>ServiceSupport</h6>
+        </div>
+        <div class="col-8 titulo1">
+               <h4 class="text-center"> Servicio De Mesa De Ayuda Soporte Técnico</h4>
+            </div>  
+            <div class="col-2 titulo1">
+               <h6> <span>
+               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-check" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <circle cx="9" cy="7" r="4" />
+                <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                <path d="M16 11l2 2l4 -4" />
+                </svg>
+               </span> Usuario  </h6>
+            </div> 
+    </div>
+    <div class="row container-fluid">
+         <div class="col-2 titulo2">
+                <span>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-school" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
+                <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" />
+                </svg>
+
+                </span>
+                <h6>Luis Diaz</h6>
+                <hr>
+                <button class="btn btn-info">
+                <span>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-dashboard" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <circle cx="12" cy="13" r="2" />
+                <line x1="13.45" y1="11.55" x2="15.5" y2="9.5" />
+                <path d="M6.4 20a9 9 0 1 1 11.2 0z" />
+                </svg>
+                </span>    
+                Inicio</button>
+              
+                <hr>
+                <button class="btn btn-info">
+                <span>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-ticket" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <line x1="15" y1="5" x2="15" y2="7" />
+                <line x1="15" y1="11" x2="15" y2="13" />
+                <line x1="15" y1="17" x2="15" y2="19" />
+                <path d="M5 5h14a2 2 0 0 1 2 2v3a2 2 0 0 0 0 4v3a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-3a2 2 0 0 0 0 -4v-3a2 2 0 0 1 2 -2" />
+                </svg>
+                </span>    
+                Tickets</button>
+                <hr>
+                <button class="btn btn-info">
+                <span>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
+            <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
+            <line x1="16" y1="5" x2="19" y2="8" />
+            </svg>
+                </span>    
+                Preferencias</button>
+                <hr>
+                <button class="btn btn-info">
+                <span>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
+            <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
+            <line x1="16" y1="5" x2="19" y2="8" />
+            </svg>
+                </span>    
+                Cerrar Sesion   
+            </button>
+      <hr>
+    </div>
+    <div class="col-8">
+        <div class="ticketnuevo">
+            <button class="btn btn-danger"> <span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-bell-ringing" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />
+            <path d="M9 17v1a3 3 0 0 0 6 0v-1" />
+            <path d="M21 6.727a11.05 11.05 0 0 0 -2.794 -3.727" />
+            <path d="M3 6.727a11.05 11.05 0 0 1 2.792 -3.727" />
+            </svg>
+            </span> <span>45</span> Ticket's Nuevos</button>
+        </div>
+        <div class="ticketnuevo2">
+            <button class="btn btn-warning"> <span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-alert-circle" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <circle cx="12" cy="12" r="9" />
+            <line x1="12" y1="8" x2="12" y2="12" />
+            <line x1="12" y1="16" x2="12.01" y2="16" />
+            </svg>
+            </span> <span>45</span> Ticket's Urgentes</button>
+        </div>
+        <div class="ticketnuevo3">
+            <button class="btn btn-success"> <span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trophy" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <line x1="8" y1="21" x2="16" y2="21" />
+                <line x1="12" y1="17" x2="12" y2="21" />
+                <line x1="7" y1="4" x2="17" y2="4" />
+                <path d="M17 4v8a5 5 0 0 1 -10 0v-8" />
+                <circle cx="5" cy="9" r="2" />
+                <circle cx="19" cy="9" r="2" />
+                </svg>
+            </span> <span>45</span> Ticket's Resueltos</button>
+        </div>
+        <div class="ticketnuevo4">
+            <button class="btn btn-success"> <span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-alarm" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <circle cx="12" cy="13" r="7" />
+            <polyline points="12 10 12 13 14 13" />
+            <line x1="7" y1="4" x2="4.25" y2="6" />
+            <line x1="17" y1="4" x2="19.75" y2="6" />
+            </svg>
+            </span> <span>45</span> Ticket's Activos</button>
+        </div>
+    </div>
+</div>
+</body>
+</html>
