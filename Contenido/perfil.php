47b1aed2d81ff0a868f1ab292ea2b8e7b9d91745
luis121526 <diazdiaz121526@gmail.com>
2021-07-26 00:52:48 -0500

    hh

--- Contenido/perfil.php
+++ Contenido/perfil.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<!--<meta http-equiv="refresh" content="30">-->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -163,7 +164,7 @@
         <h6 class="text-center">ServiceSupport, todos los derechos reservados</h6>
     </div>
     <div class="col-2 titulo1">
-        
+
     </div>
 </div>
 
